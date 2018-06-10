3b94534092418262ccbba0805693288e44d3334c
elbaro <elbaro@github>
2018-06-11 01:46:05 +0900

    Visualize indexed images

--- tinder/visualize.py
+++ tinder/visualize.py
@@ -2,9 +2,11 @@ import numpy as np
 import torch
 import matplotlib
 import matplotlib.pyplot as plt
+import pylab
 
+cmap = plt.get_cmap('Accent', 256)
 
-def show_imgs(imgs, rows, cols):
+def show_imgs(imgs, rows, cols, is_indexed:[bool]=None):
     """
     Show an image grid in a popup window.
 
@@ -18,13 +20,16 @@ def show_imgs(imgs, rows, cols):
         imgs
         rows
         cols
+        is_indexed ([bool]): a list of flags that represents whether images are indexed images.
     """
 
     assert len(imgs) <= rows*cols
+    if is_indexed is None: is_indexed = [False] * len(imgs)
+    assert len(imgs) == len(is_indexed)
 
     fig = plt.figure()
 
-    for i, img in enumerate(imgs):
+    for i, (img, cmap) in enumerate(zip(imgs, is_indexed)):
         if type(img) == torch.Tensor:
             img = img.detach().cpu().numpy()
 
@@ -32,6 +37,25 @@ def show_imgs(imgs, rows, cols):
             img = np.transpose(img, axes=(1,2,0))
 
         fig.add_subplot(rows,cols,i+1)
-        plt.imshow(img)
+
+        if is_indexed:
+            plt.imshow(img, cmap=cmap)
+        else:
+            plt.imshow(img)
 
     plt.show()
+
+
+# def colorize(imgs, colormap=None):
+#     """Colorize indexed images.
+
+#     Useful for visualizing segmentation labels.
+
+#     Args:
+#         imgs ([type]): indexed images.
+#         colormap ([type], optional): Defaults to None. [description]
+#     """
+
+#     vals = np.linspace(0,1,256)
+#     np.random.shuffle(vals)
+#     cmap = plt.cm.colors.ListedColormap(plt.cm.jet(vals))
