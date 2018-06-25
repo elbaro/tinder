42ff92baed8556bdf1f496c439162c7edb97a33f
elbaro <elbaro@noreply.github.com>
2018-06-26 05:07:36 +0900

    Fix test

--- tinder/test/test_batch.py
+++ tinder/test/test_batch.py
@@ -2,7 +2,7 @@ import tinder
 
 
 def test_batch():
-    iterable = tinder.batch([1, 2, 3, 4], 3)
+    iterable = tinder.batch.iter_from_iter([1, 2, 3, 4], 3)
     iterator = iter(iterable)
     assert next(iterator) == [1, 2, 3]
     assert next(iterator) == [4]
