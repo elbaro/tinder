0ae8a7387875e59e6f9150f1dadd5486af61a822
elbaro <elbaro@noreply.github.com>
2018-07-07 16:53:27 +0900

    add hash100

--- tinder/__init__.py
+++ tinder/__init__.py
@@ -12,6 +12,6 @@ from .visualize import show_imgs
 from .metrics import instance_segmentation_iou, semantic_segmentation_iou
 from .nn import WeightScale, PixelwiseNormalize, MinibatchStddev, loss_wgan_gp
 from .layers import AssertSize, Flatten, View
-from .dataset import hash_group, DataLoaderIterator, BalancedDataLoader
+from .dataset import hash100, DataLoaderIterator, BalancedDataLoader
 from .monitor import Stat, Stats
 from .queue import RedisQueue, RabbitConsumer, RabbitProducer
