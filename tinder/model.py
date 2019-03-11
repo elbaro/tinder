a214774842f45f46fe4a1c07a1a7a88ede538cff
elbaro <elbaro@users.noreply.github.com>
2019-03-11 20:14:29 +0900

    Use sacred metric

--- tinder/model.py
+++ tinder/model.py
@@ -2,7 +2,6 @@ from types import SimpleNamespace
 import tinder
 import torch
 import tqdm
-import tensorboardX
 from colorama import Fore, Style
 
 
@@ -17,7 +16,7 @@ class Model(object):
                  name,
                  net,
                  opt=None,
-                 log_dir=None,
+                 logger_fn=None,
                  weight_dir=None,
                  load='latest'):
 
@@ -35,10 +34,7 @@ class Model(object):
             step=0,
         )
 
-        if log_dir is not None:
-            self.tb = tensorboardX.SummaryWriter(log_dir)
-        else:
-            self.tb = None
+        self.logger_fn = logger_fn
 
         if weight_dir is not None:
             self.saver = tinder.saver.Saver(weight_dir, name)
@@ -58,26 +54,22 @@ class Model(object):
     def train_epoch(self,
                     loader,
                     train_minibatch_fn,
-                    is_logging=True,
-                    manual_opt=False):
+                    logging='step',
+                    manual_opt=False,
+                    interactive=False):
         assert (self.opt is not None) or manual_opt
-        if is_logging and (self.tb is None):
-            assert 'log_dir is not given'
-
         self.net.train()
 
-        loader = tqdm.tqdm(loader, leave=False)
+        if interactive:
+            loader = tqdm.tqdm(loader, leave=False)
         step = self.bundle.step
 
         n = 0
         metrics = {}
         for batch in loader:
-            assert isinstance(batch, dict)
             step += 1
-
-            batch_size = batch['batch_size'] = len(batch)
+            batch_size = len(batch)
             n += batch_size
-
             result = train_minibatch_fn(batch)
 
             if not manual_opt:
@@ -85,21 +77,27 @@ class Model(object):
                 result['loss'].backward()
                 self.opt.step()
 
-            if is_logging:
-                result['loss'] = result['loss'].item()
+            for (k, v) in result.items():
+                if torch.is_tensor(v):
+                    result[k] = v.item()
+
+            if (self.logger_fn is not None) and logging == 'step':
+                for (k, v) in result.items():
+                    # self.tb.add_scalar(k, v, step)
+                    self.logger_fn('train.'+k, v, step)
+
+            if interactive:
                 msg = "[train]"
                 for (k, v) in result.items():
-                    if torch.is_tensor(v):
-                        v = v.item()
-
-                    self.tb.add_scalar(k, v, step)
-                    msg += f"    {k} = {v:8.4}"
-                    v *= batch_size
-                    if k in metrics:
-                        metrics[k] += v
-                    else:
-                        metrics[k] = v
+                    msg += f"    {k} = {v:8.4f}"
                 loader.set_description(msg)
+
+            for (k, v) in result.items():
+                v *= batch_size
+                if k in metrics:
+                    metrics[k] += v
+                else:
+                    metrics[k] = v
 
         self.bundle.step = step
         self.bundle.epoch += 1
@@ -110,15 +108,38 @@ class Model(object):
         # }
         for k in metrics:
             metrics[k] /= n
+
+        if (self.logger_fn is not None) and logging == 'epoch':
+            for (k, v) in metrics.items():
+                self.logger_fn('train.'+k, v, step)
+
+        # return per-sample metric
         return metrics
 
     def eval(self, loader, eval_minibatch_fn):
         self.net.eval()
 
+        n = 0
+        metrics = {}
         for batch in loader:
-            if isinstance(batch, dict):
-                batch['batch_size'] = len(batch)
-            log = eval_minibatch_fn(batch)
+            n += len(batch)
+            result = eval_minibatch_fn(batch)
+            for (k, v) in result.items():
+                if torch.is_tensor(v):
+                    v = v.item()
+                v *= len(batch)
+                if k in metrics:
+                    metrics[k] += v
+                else:
+                    metrics[k] = v
+        for k in metrics:
+            metrics[k] /= n
+
+        step = self.bundle.step
+        if self.logger_fn is not None:
+            for (k, v) in metrics.items():
+                self.logger_fn('eval.'+k, v, step)
+        return metrics
 
     def train(self,
               epochs,
@@ -129,35 +150,48 @@ class Model(object):
               score_col=None,
               manual_opt=False,
               eval_loader=None,
-              eval_minibatch_fn=None):
+              eval_minibatch_fn=None,
+              interactive=False):
 
-        for epoch in tqdm.trange(self.bundle.epoch + 1, epochs + 1):
+        if interactive:
+            YELLOW = Fore.YELLOW
+            GREEN = Fore.GREEN
+            RESET_ALL = Style.RESET_ALL
+        else:
+            YELLOW = ''
+            GREEN = ''
+            RESET_ALL = ''
+
+        # for epoch in :
+        for1 = tqdm.trange(self.bundle.epoch + 1, epochs + 1,
+                           desc='epoch') if interactive else range(self.bundle.epoch + 1, epochs + 1)
+        for epoch in for1:
             metrics = self.train_epoch(train_loader, train_minibatch_fn,
-                                       logging == 'step', manual_opt)
+                                       logging, manual_opt, interactive=interactive)
 
             train_msg = ""
             for (k, v) in metrics.items():
-                train_msg += f"{k} = {v:8.4}    "
+                train_msg += f"{k} = {v:8.4f}    "
 
             if (eval_loader is not None) and (eval_minibatch_fn is not None):
-                self.eval(eval_loader, eval_minibatch_fn)
+                metrics = self.eval(eval_loader, eval_minibatch_fn)
                 eval_msg = ""
                 for (k, v) in metrics.items():
-                    eval_msg += f"{k} = {v:8.4}    "
+                    eval_msg += f"{k} = {v:8.4f}    "
 
                 tqdm.tqdm.write(
-                    f'epoch {epoch:3d} [train] {Fore.YELLOW}{train_msg}{Style.RESET_ALL}'
-                    + f'    [valid] {Fore.GREEN}{eval_msg}{Style.RESET_ALL}')
+                    f'epoch {epoch:3d} [train] {YELLOW}{train_msg}{RESET_ALL}'
+                    + f'    [valid] {GREEN}{eval_msg}{RESET_ALL}')
             else:
                 tqdm.tqdm.write(
-                    f'epoch {epoch:3d} [train] {Fore.YELLOW}{train_msg}{Style.RESET_ALL}'
+                    f'epoch {epoch:3d} [train] {YELLOW}{train_msg}{RESET_ALL}'
                 )
 
             if (self.saver is not None) and (epoch % save_epoch_interval) == 0:
                 self.saver.save(
                     self.bundle,
                     epoch=self.bundle.epoch,
-                    score=metrics['score_col'] if
+                    score=metrics[score_col] if
                     (score_col is not None) else 0.0)
 
     def infer(self, net, loader, infer_minibatch_fn, id_col='id'):
@@ -166,7 +200,7 @@ class Model(object):
 
         for batch in loader:
             assert isinstance(batch, dict)
-            assert 'id' in batch
+            assert id_col in batch
 
             batch['batch_size'] = len(batch)
             minibatch_result = infer_minibatch_fn(batch)
